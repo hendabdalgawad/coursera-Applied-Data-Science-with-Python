a3294426a06924245ef2b53b109a68b46ab0c26b
hendabdalgawad <44335630+hendabdalgawad@users.noreply.github.com>
2018-11-28 12:50:23 +0200

    Add files via upload

---
+++ Capstone_Projcet_Notebook.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# # Data Science Specialization Capstone Project
+
+# I'm going to use what I've leanred already to build my first Data Science Project. So, let's get started!
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
